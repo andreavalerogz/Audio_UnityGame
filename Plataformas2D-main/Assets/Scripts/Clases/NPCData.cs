9021724087689087aa1b5ae6ec5140112105e859
andreavalerog <andreavaleroadm@gmail.com>
2026-05-27 20:55:15 +0200

    corrupt files

--- Plataformas2D-main/Assets/Scripts/Clases/NPCData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NPCData", menuName = "Scriptable Objects/NPCData")]
-public class NPCData : ScriptableObject
-{
-    public string characterName;
-    [TextArea] public string[] lines;
-}
